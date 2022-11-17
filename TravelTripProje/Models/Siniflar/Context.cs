b580795530d0aa46402760bd5bf871fedf6a5953
Onur Doğru <onurdogruiletisim@gmail.com>
2022-11-17 11:32:36 +0300

    create project

---
+++ TravelTripProje/Models/Siniflar/Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace TravelTripProje.Models.Siniflar
+{
+    public class Context : DbContext
+    {
+        public DbSet<Admin> Admins { get; set; } //<Admin> hangi sınıfı kullandığımızı ifade eder. Admins ise veri tabanındaki ismidir.
+        public DbSet<AdresBlog> AdresBlogs { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Hakkimizda> Hakkimizdas { get; set; }
+        public DbSet<iletisim> iletisims { get; set; }
+        public DbSet<Yorumlar> Yorumlars { get; set; }
+        
+        
+
+
+    }
+}
